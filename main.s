16494b335eddb19b17394b6b5928405b9c774bf0
Martha30 <53914325+Martha30@users.noreply.github.com>
2020-05-27 11:54:50 -0600

    Create main.s
    main

---
+++ main.s
@@ -0,0 +1,15 @@
+@Universidad del Valle de Guatemala
+@Martha Gómez 19501
+@Daniela Batz 19284
+@Proyecto Final Assembler 
+@Grupo 
+@Temario 3 Juego Pi-DOTS
+
+@Main
+
+.data
+.align 2
+
+bienvenida:     .asciz "Bienvenido a Pi-DOTS\n"
+salida:         .asciz "Gracias por jugar\n"
+
