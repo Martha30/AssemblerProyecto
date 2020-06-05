bedd0cfd8d9ba0a1003d08d7535862d3c2f4eb1b
DanBea2019 <53918887+DanBea2019@users.noreply.github.com>
2020-06-05 17:28:35 -0600

    cambios en el main

--- main.s
+++ main.s
@@ -5,11 +5,32 @@
 @Grupo 
 @Temario 3 Juego Pi-DOTS
 
+.text
+ .align 2
+ .global main
+ .func main
 @Main
+main:
+	ldr r10,=columna1
 
 .data
 .align 2
-
+formato: asciz "%d\n"
 bienvenida:     .asciz "Bienvenido a Pi-DOTS\n"
-salida:         .asciz "Gracias por jugar\n"
+posicion: .asciz
+columna1: .word 1 0,0,0,0,0
+columna2: .word 2 0,0,0,0,0
+columna3: .word 3 0,0,0,0,0
+columna4: .word 4 0,0,0,0,0
+columna5: .word 5 0,0,0,0,0
+opcion: .word 0
 
+salida:         .asciz "Gracias por jugar\n"
+presentacion: . asciz " _____     _____              ______       ____ 
+(  __ \   (_   _)            (_  __ \     / __ \   (___  ___)  / ____\ 
+ ) )_) )    | |    ________    ) ) \ \   / /  \ \      ) )    ( (___   
+(  ___/     | |   (________)  ( (   ) ) ( ()  () )    ( (      \___ \  
+ ) )        | |                ) )  ) ) ( ()  () )     ) )         ) ) 
+( (        _| |__             / /__/ /   \ \__/ /     ( (      ___/ /  
+/__\      /_____(            (______/     \____/      /__\    /____/   
+                                                                      "
