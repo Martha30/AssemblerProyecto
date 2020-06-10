57a90785f65cae2e93cb7f45a7ac4471e340e75c
DanBea2019 <53918887+DanBea2019@users.noreply.github.com>
2020-06-09 18:11:36 -0600

    Cambios finales en la subrutina

--- subrutina3.s
+++ subrutina3.s
@@ -1,33 +1,86 @@
-@Universidad del Valle de Guatemala
-@Martha Gómez 19501
-@Daniela Batz 19284
-@Proyecto Final Assembler 
-@Grupo 8
-@Temario 3 Juego Pi-DOTS
-@Subrutina para crear aleatorios
+@----------------------------------
 
 .data
-seed:	.word 1
-const1:	.word	2
-const2:	.word	2
+
+@ See /usr/include/arm-linux-gnueabihf/asm/unistd.h
+@ See /usr/include/arm-linux-gnueabihf/bits/fcntl-linux.h
+
+    .equ create,     8
+         .equ Mode, 0644       @ -rw-r--r--
+    .equ open,       5
+         .equ Rd,   00
+         .equ Wr,   01
+         .equ RdWr, 02
+         .equ Apnd, 02000
+    .equ read,       3
+    .equ write,      4
+    .equ close,      6
+    .equ sync,       36
+    .equ exit,       1
+    .equ sfile,      187
+
+@----------------------------------
+
+.balign 4
+dir_file:
+    .asciz "/dev/urandom"
+
+.balign 4
+Open:
+    .word dir_file, RdWr | Apnd, open
+
+.balign 4
+Buf:
+    .word 0 
+
+.balign 4
+Read:
+    .word Buf, 1, read
+
+.balign 4
+format:
+    .asciz "%3d\n"
+error : .asciz "      CUIDADO: Opcion fuera de rango! "
+@----------------------------------
 
 .text
-.global myrand, mysrand
 
-myrand:
-	push {lr}
-	ldr r1, =seed	@puntero a semilla
-	ldr r0, [r1]	@leer valor
-	ldr r2, [r1,#4]	@leer const1
-	mul r3, r0, r2	@r3=seed*1103515245
-	ldr r0, [r1,#8]	@leer const2
-	add r0, r0, r3	@r0=r3+12345
-	str r0, [r1]	@guardo en variable seed
-	lsl r0, #1	@devuelve seed>>16&0x7FFF
-	lsr r0, #17
-	pop {lr}
-mysrand:
-	push {lr}
-	ldr r1, =seed
-	str r0, [r1]
-	pop {lr}
+.global rangoOut
+rangoOut:
+    push {lr}
+    ldr r0, =error
+    bl puts
+    pop {lr}
+    mov pc,lr
+
+.global aleatorios
+aleatorios:
+
+@    push   {r4, r5, r7, lr}     folowing AAPCS
+
+     ldr    r3, =Open            @ load address
+     ldm    r3, {r0, r1, r7}     @ load registers
+     svc    #0                   @ OS opens file
+     mov    r4, r0               @ save fd in r4
+
+     ldr    r3, =Read            @ load address
+     ldm    r3, {r1, r2, r7}     @ load registers
+     svc    #0                   @ OS reads file
+
+     mov    r0, r4               @ move fd in r0
+     mov    r7, #close           @ num for close
+     svc    #0                   @ OS closes file
+
+@     ldr    r0, =format          adress of format
+       @ load byte
+@     bl     printf               @ C() print byte
+@     mov    r0, #0               @ 0 = success
+
+exit:
+
+@     pop    {r4, r5, r7, lr}     @ folowing AAPCS
+     ldr    r1, =Buf             @ addr of byte red
+     ldr    r0, [r1]      
+     mov    pc, lr                   @ Exit if use gcc as linker
+	 
+	 
